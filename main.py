a8488ebbab585629b4ac6c296abec5d3323b7d11
泰盛金子 <yasumorikaneko@yasumorikaneko-no-MacBook-Pro.local>
2020-07-15 23:31:18 +0900

    return json

--- main.py
+++ main.py
@@ -28,80 +28,50 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 
 from PIL import Image, ImageFilter
+@app.route('/')
+def hello():
+    return 'Hello Heroku_Flask'
 
 
+def pdf_translation(filename):
 # 実行ファイルと同じディレクトリにあるsample1.pdfをpdf2imageでpngに変換する
-path = os.path.join(os.path.dirname(__file__), 'sample1.pdf')
-images = convert_from_path(path)
-# pngファイルをTesserocrでテキストデータに変換し、text_listに格納
-text_list = ['a'] *len(images)
-text_translated = ['b'] * len(images)
-for i in range(len(images)):
-    images[i].save('test{}.png'.format(i), 'png')
-    api = PyTessBaseAPI(psm=PSM.AUTO, lang='eng')
-    api.SetImageFile('test{}.png'.format(i))
-    text_list[i] = api.GetUTF8Text()
+    path = os.path.join(os.path.dirname(__file__), filename)
+    images = convert_from_path(path)
+    # pngファイルをTesserocrでテキストデータに変換し、text_listに格納
+    text_list = ['a'] *len(images)
+    text_translated = ['b'] * len(images)
+    for i in range(len(images)):
+        images[i].save('test{}.png'.format(i), 'png')
+        api = PyTessBaseAPI(psm=PSM.AUTO, lang='eng')
+        api.SetImageFile('test{}.png'.format(i))
+        text_list[i] = api.GetUTF8Text()
 
-# config.jsonに格納されているAPIキーを使用可能に
-path_json= os.path.join(os.path.dirname(__file__), "config.json")
-api = Config(path_json)
-api_key = api.api_key
+    # config.jsonに格納されているAPIキーを使用可能に
+    path_json= os.path.join(os.path.dirname(__file__), "config.json")
+    api = Config(path_json)
+    api_key = api.api_key
 
-key = "?key=" + api_key
-url = "https://translation.googleapis.com/language/translate/v2"
-language = "&source=en&target=ja"
-# google translationでtext_listのテキストデータを日本語へ翻訳し、text_translatedにページごとに格納
-for i in range(len(images)):
-    q = "&q=" + text_list[i]
-    url_a = url + key + q + language
-    rr = requests.get(url_a)
-    unit_aa = json.loads(rr.text)
-    if "data" in unit_aa:
-        result = unit_aa['data']['translations'][0]['translatedText']
-    else:
-        result = "このページでは翻訳エラーが発生しました"
-    text_translated[i] = result
+    key = "?key=" + api_key
+    url = "https://translation.googleapis.com/language/translate/v2"
+    language = "&source=en&target=ja"
+    # google translationでtext_listのテキストデータを日本語へ翻訳し、text_translatedにページごとに格納
+    for i in range(len(images)):
+        q = "&q=" + text_list[i]
+        url_a = url + key + q + language
+        rr = requests.get(url_a)
+        unit_aa = json.loads(rr.text)
+        text_translated[i] = unit_aa
+        text_translated[i].update(pagenumber=i+1)
+    return text_translated
 
-# 日本語のフォントの決定
-pdfmetrics.registerFont(UnicodeCIDFont('HeiseiMin-W3'))
-# 翻訳後のpdfファイルのタイトル作成
-c = canvas.Canvas("test.pdf")
-# text_translatedからtest.pdfに書き込み・表示
-for j in range(len(text_translated)):
-    text_case = textwrap.wrap(text_translated[j], 50)
-    c.setFont('HeiseiMin-W3', 10.5)
-    for i in range(len(text_case)):
-        c.drawString(10, 830 - i * 12, text_case[i])
-    c.showPage()
-c.save()
+pdf_translation(filename='sample1.pdf')
 
-# pdfを表示用にpngにする（pngファイルはバラバラ）
-path_test = os.path.join(os.path.dirname(__file__), 'test.pdf')
-result_images = convert_from_path(path_test)
-for i in range(len(result_images)):
-    result_images[i].save('result{}.png'.format(i), 'png')
-    imgs = Image.open('result{}.png'.format(i))
-#　結果の画像（pngファイル）を連結させて開く
-def get_concat_v(im1, im2):
-    dst = Image.new('RGB', (im1.width, im1.height + im2.height))
-    dst.paste(im1, (0, 0))
-    dst.paste(im2, (0, im1.height))
-    return dst
-
-if len(result_images) > 2:
-    combined = get_concat_v(Image.open('result0.png'), Image.open('result1.png'))
-    combined.save('combined0.png')
-    for j in range(len(result_images)-2):
-        combined = get_concat_v(Image.open('combined{}.png'.format(j)), Image.open('result{}.png'.format(j+2)))
-        combined.save('combined{}.png'.format(j+1))
-    Image.open('combined{}.png'.format(len(result_images)-2)).show()
-elif len(result_images) == 2:
-    combined = get_concat_v(Image.open('result0.png'), Image.open('result1.png'))
-    combined.save('combined0.png')
-    Image.open('combined0.png').show()
-else:
-    IMage.open('result0.png').show()
-
+"""
+こんな感じのjsonが格納された配列が生成されている（pagenumberは1からスタート）
+[{'data':{'translations':[{'translatedText':'Web工学'}]}'pagenumber':1}, 
+{'data':{'translations':[{'translatedText':'深層学習'}]}, 'pagenumber':2}]
+"""
 if __name__ == "__main__":
-    app.run(debug=False)
+    
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)),debug=False)
     
