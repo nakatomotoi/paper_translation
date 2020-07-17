80d2fea4667e0c758052985f19ce8e65dd01cfe7
泰盛金子 <yasumorikaneko@yasumorikaneko-no-MacBook-Pro.local>
2020-07-18 01:10:10 +0900

    main.py renewed

--- main.py
+++ main.py
@@ -24,11 +24,12 @@ from PIL import Image, ImageFilter
 def hello():
     return 'Hello Heroku_Flask'
 
-
+@app.route(‘/translation’)
 def pdf_translation(filename):
     # 実行ファイルと同じディレクトリにあるsample1.pdfをpdf2imageでpngに変換する
-    path = os.path.join(os.path.dirname(__file__), filename)
-    images = convert_from_path(path)
+    #path = os.path.join(os.path.dirname(__file__), filename)
+    #convert_from_bytesを使うようにしました
+    images = convert_from_bytes(open(filename,'rb').read())
     # pngファイルをTesserocrでテキストデータに変換し、text_listに格納
     text_list = ['a'] * len(images)
     text_translated = ['b'] * len(images)
@@ -51,14 +52,24 @@ def pdf_translation(filename):
     url = "https://translation.googleapis.com/language/translate/v2"
     language = "&source=en&target=ja"
     # google translationでtext_listのテキストデータを日本語へ翻訳し、text_translatedにページごとに格納
+    dict_list= [{'page_number':1}]
+    for i in range(len(images)-1):
+        dict_list.append({'page_number':i+2})
     for i in range(len(images)):
         q = "&q=" + text_list[i]
         url_a = url + key + q + language
         rr = requests.get(url_a)
         unit_aa = json.loads(rr.text)
         text_translated[i] = unit_aa
-        text_translated[i].update(pagenumber=i+1)
-    return text_translated
+        if 'data' in text_translated[i]:
+            dict_list[i].update(translated_text=text_translated[i]['data']['translations'][0]['translatedText'])
+        else:
+            dict_list[i].update(translated_text="このページでは翻訳エラーが発生しました")
+
+
+    dict_return = {}
+    dict_return.update(data=dict_list)
+    return dict_return
 
 
 pdf_translation(filename='sample1.pdf')
@@ -68,6 +79,7 @@ print(pdf_translation(filename='sample1.pdf'))
 こんな感じのjsonが格納された配列が生成されている（pagenumberは1からスタート）
 [{'data':{'translations':[{'translatedText':'Web工学'}]}'pagenumber':1}, 
 {'data':{'translations':[{'translatedText':'深層学習'}]}, 'pagenumber':2}]
+{'data': [{'page_number': int, 'translated_text': string}]}
 """
 if __name__ == "__main__":
     
